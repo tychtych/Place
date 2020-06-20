06c04aa640f15d3d0926549ff549a6d67a8610da
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-06-20 16:50:27 -0700

    fixed method for finding a single user, added parser

--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,21 @@ const router = express.Router();
 
 const usersPath = path.join(__dirname, '../data/users.json');
 const usersData = fs.readFileSync(usersPath, { encoding: 'utf8' });
+const parsedUsersData = JSON.parse(usersData);
 
 // get all users
 router.get('/', (req, res) => {
-  res.send(usersData);
+  res.send(parsedUsersData);
 });
 
 // get a single user
 router.get('/:id', (req, res) => {
-  // eslint-disable-next-line no-underscore-dangle
-  const foundUser = usersData.some((user) => user._id === req.params.id);
-  if (foundUser) {
-    // eslint-disable-next-line no-underscore-dangle
-    res.send(usersData.filter((user) => user._id === req.params.id));
-  } else {
+  if (!parsedUsersData[req.params.id]) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
+    return;
   }
+  // eslint-disable-next-line no-underscore-dangle
+  res.send(parsedUsersData.find((user) => user._id === req.params.id));
 });
 
 module.exports = router;
