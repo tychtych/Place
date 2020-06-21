3bea266948fa65014d0b35933740e9c771f2ac2b
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-06-21 16:18:42 -0700

    added minified project's files, req.params.id declaration is carried outside the logic

--- routes/users.js
+++ routes/users.js
@@ -8,19 +8,16 @@ const usersPath = path.join(__dirname, '../data/users.json');
 const usersData = fs.readFileSync(usersPath, { encoding: 'utf8' });
 const parsedUsersData = JSON.parse(usersData);
 
-// get all users
 router.get('/', (req, res) => {
   res.send(parsedUsersData);
 });
 
-// get a single user
 router.get('/:id', (req, res) => {
-  // TODO array is array check
-  if (!parsedUsersData[req.params.id]) {
+  const userId = req.params.id;
+  if (!userId) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
     return;
   }
-
   // eslint-disable-next-line no-underscore-dangle
   res.send(parsedUsersData.find((user) => user._id === req.params.id));
 });
