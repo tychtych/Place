517658391373a974ba7fc2d460eeeea91963960e
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-06-23 16:48:18 -0700

    used async function for getting data, applied the method for response

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 const router = express.Router();
 
 const cardsPath = path.join(__dirname, '../data/cards.json');
-const cardData = fs.readFileSync(cardsPath, { encoding: 'utf8' });
 
-router.get('/', (req, res) => {
-  res.send(cardData);
+async function getCardsData() {
+  try {
+    // TODO separate error catching while parsing
+    const parsedData = JSON.parse(await fsPromises.readFile(cardsPath, { encoding: 'utf-8' }));
+    return parsedData;
+  } catch (error) {
+    throw new Error('Ошибка чтения файла');
+  }
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const cardData = await getCardsData();
+    res.send(cardData);
+  } catch (e) {
+    res.status(500)
+      .send({ message: e.message });
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 const router = express.Router();
-
 const usersPath = path.join(__dirname, '../data/users.json');
-const usersData = fs.readFileSync(usersPath, { encoding: 'utf8' });
-const parsedUsersData = JSON.parse(usersData);
 
-router.get('/', (req, res) => {
-  res.send(parsedUsersData);
+async function getUsersData() {
+  try {
+    // TODO separate error catching while parsing
+    const parsedData = JSON.parse(await fsPromises.readFile(usersPath, { encoding: 'utf-8' }));
+    return parsedData;
+  } catch (error) {
+    throw new Error('Ошибка чтения файла');
+  }
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await getUsersData();
+    res.send(userData);
+  } catch (e) {
+    res.status(500)
+      .send({ message: e.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // eslint-disable-next-line no-underscore-dangle
-  if (!parsedUsersData.find((user) => user._id === req.params.id)) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-  }
-  if (Array.isArray(parsedUsersData)) {
-    // eslint-disable-next-line no-underscore-dangle
-    res.send(parsedUsersData.find((user) => user._id === req.params.id));
+router.get('/:id', async (req, res) => {
+  try {
+    const usersData = await getUsersData();
+    if (Array.isArray(usersData)) {
+      const foundUser = await usersData
+        // eslint-disable-next-line no-underscore-dangle
+        .find((user) => user._id === req.params.id);
+      if (!foundUser) {
+        res.status(404)
+          .send({ message: 'Нет пользователя с таким id' });
+      }
+      // eslint-disable-next-line no-underscore-dangle
+      res.send(foundUser);
+    } else {
+      res.status(500)
+        .send({ message: 'Некорректные данные' });
+    }
+  } catch (e) {
+    res.status(500)
+      .send({ message: e.message });
   }
 });
 
